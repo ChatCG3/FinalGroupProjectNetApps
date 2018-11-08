fe5219a9bbf19456dee22e4e3df5af576a4b664b
Devdawg96 <44882224+Devdawg96@users.noreply.github.com>
2018-11-08 17:53:56 -0500

    Add files via upload

---
+++ DeleteQuery.java
@@ -0,0 +1,52 @@
+package dbhelpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteQuery {
+	
+	private Connection connection;
+	
+	public DeleteQuery(String dbName, String uname, String pwd) {
+		
+		String url = "jdbc:mysql://loaclhost:3306/" + dbName;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			this.connection = DriverManager.getConnection(url, uname, pwd);
+			
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void doDelete(int productID) {
+		
+		// set up String to hold query
+		String query = "delete from product where productID =";
+		
+		// create a prepared statement
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			
+			// fill in the prepared statement
+			ps.setInt(1, productID);
+			
+			// execute the query
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+
+}
